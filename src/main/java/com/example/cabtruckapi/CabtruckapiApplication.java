e35f2b9b4725f71d1d11b1448d0ad869dff70362
Vitor Reis <vitordesouzareis@gmail.com>
2026-05-05 20:08:53 -0300

    Adiciona estrutura inicial do projeto com Maven, incluindo arquivos de configuração, classes de entidade e testes

---
+++ src/main/java/com/example/cabtruckapi/CabtruckapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabtruckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabtruckapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabtruckapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cabtruckapi/CabtruckapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabtruckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabtruckapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
